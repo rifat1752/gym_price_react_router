fb2ba63418b89025d22de339bd9079e0ea7588ff
rifat1752 <rifathossain1752@gmail.com>
2023-12-02 01:02:22 +0600

    navbar done

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -1,10 +1,34 @@
+import PropTypes from 'prop-types';
 
 const PriceOption = ({priceOption}) => {
-    return (
-        <div>
-            PriceOption
+    const {name,img,price,gymFeatures} = priceOption;
+   console.log(gymFeatures)
+  return (
+    <div className='flex justify-center'>
+      <div className="card w-72 bg-stone-300 my-5 shadow-xl">
+        <figure>
+          <img className='w-full'
+            src={img}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {name}
+            <div className="w-16 badge badge-secondary">{price}$</div>
+          </h2>
+          <div className="card-actions justify-end">
+            {/* <div className="badge badge-outline">Fashion</div> */}
+            {gymFeatures.map(feature=><div key={feature} className="badge badge-outline flex-grow">{feature}</div>)}
+          </div>
+          <button className="w-24 btn mt-5 justify-items-end   btn-info">Subscribe</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
+PriceOption.propTypes ={
+  priceOption: PropTypes.object.isRequired
+}
 export default PriceOption;
--- src/Components/PriceOption/PriceOptions.jsx
+++ src/Components/PriceOption/PriceOptions.jsx
@@ -11,9 +11,13 @@ const PriceOptions = () => {
     },[])
 
     return (
-        <div>
-        {priceOptions.map(priceOption=> <PriceOption key={priceOption.id}  priceOption={priceOption}></PriceOption>)}
+        <div className="mt-5">
+            <div className="text-center text-4xl font-bold ">   Services </div>
+            <div className="my-5 grid md:grid-cols-2 lg:grid-cols-3 bg-slate-300 ">
+            {priceOptions.map(priceOption=> <PriceOption key={priceOption.id}  priceOption={priceOption}></PriceOption>)}
         </div>
+            </div>
+      
     );
 };
 
